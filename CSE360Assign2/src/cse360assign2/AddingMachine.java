6b79e1cd6997960f8432dca0f024045bbe21f389
Kristian Avila <kavila5@asu.edu>
2019-10-08 21:33:40 -0700

    Final Version
    All class methods implemented and are tested for functionality. Now
    should perform simple Addition and Subtraction and be able to print the
    total to the user and print a string describing the calculations
    history.

--- CSE360Assign2/src/cse360assign2/AddingMachine.java
+++ CSE360Assign2/src/cse360assign2/AddingMachine.java
@@ -17,7 +17,12 @@ package cse360assign2;
 
 public class AddingMachine
 {
+	// Two global variables declared, one to hold the total of the calculation
+	// being done and another string variable to hold the string of the 
+	// calculations being performed
 	private int total;
+	private String calcHistory;
+	
 	
 	/** Creates an constructor method for the class AddingMachine and will
 	 * initialize the global variable total to 0. 
@@ -25,6 +30,7 @@ public class AddingMachine
 	public AddingMachine()
 	{
 		total = 0;  // not needed - included for clarity
+		calcHistory = "0"; // set string equal to 0 for consistent output
 	}
 	
 	/** Gets the total for the calculation being performed.
@@ -33,43 +39,52 @@ public class AddingMachine
 	*/
 	public int getTotal()
 	{
-		return 0;
+		return total;
 	}
 	
-	/** the add method will take a integer variable called value that will
-	 * be added to the global variable total.
-	 * @param value
+	/** The add method will take a integer variable called value that will
+	 * be added to the global variable total. It also will add the calculation
+	 * to the calcHistory global variable to be able to print the history.
+	 * @param value - the value will be added to the global variable total
+	 * keep track of the calculation
 	*/
 	public void add(int value)
 	{
-		
+		total = total + value;
+		calcHistory = calcHistory + " + " + value;
 	}
 	
-	/** the subtract method will take a integer variable called value that
-	 * will be subtracted from the global variable total.
-	 * @param value
+	/** The subtract method will take a integer variable called value that
+	 * will be subtracted from the global variable total. It also will add
+	 * the calculation to the calcHistory global variable to be able to 
+	 * print the history.
+	 * @param value -  the value will be added to the global variable total
+	 * keep track of the calculation
 	*/
 	public void subtract(int value)
 	{
-		
+		total = total - value;
+		calcHistory = calcHistory + " - " + value;
 	}
 	
 	/** the toString method will turn the global variable total and convert it
 	 * to a string so the calculation is able to be printed to the user in a
 	 * way that is easy to read.
-	 * @return A string representing the total of the calculation being
+	 * @return A string representing the history of the calculations being
 	 * performed
 	*/
 	public String toString()
 	{
-		return "";
+		return calcHistory;
 	}
 	
 	/** The clear method has no parameters and no return value. This method
-	 * will reset the global variable total back to 0.
+	 * will reset the global variable total back to 0. Meant to reset values
+	 * to allow another calculation to be performed.
 	*/
 	public void clear()
 	{
-		
+		total = 0;
+		calcHistory = "0";
 	}
 }
